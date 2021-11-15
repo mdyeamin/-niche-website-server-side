a11a4e98f94149180f094f20c883ad5f9799d424
Md Yeamin <ahmenyeamin05@gmail.com>
2021-11-16 01:15:59 +0600

    add

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lqhl2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
+console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
